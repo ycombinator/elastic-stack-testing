60b7e1f74f528f29eea0284c14531c9a32c7bbff
Liza Dayoub <liza.dayoub@elastic.co>
2018-02-06 16:01:42 -0700

    Update buildenv.sh

--- buildenv.sh
+++ buildenv.sh
@@ -2,12 +2,12 @@
 #
 # @author: Liza Dayoub
 
-export ES_BUILD_LOCAL=true
+export AIT_RUN_LOCAL=true
 
 export ES_BUILD_URL=staging.elastic.co/5.6.3-d64d30d8
 export ES_BUILD_PKG_EXT=tar
 
 export AIT_ANSIBLE_PLAYBOOK=install_no_xpack
-export AIT_ANSIBLE_SCRIPT=setup_vagrant_vm
+export AIT_VM=setup_vagrant_vm
 
 source jenkins_build.sh
