360bdf96cbe22587653d94f698e078c1e06bd8bf
Liza Dayoub <liza.dayoub@elastic.co>
2018-02-12 13:14:36 -0700

    Change to correct vagrant setup

--- buildenv.sh
+++ buildenv.sh
@@ -8,6 +8,6 @@ export ES_BUILD_URL=staging.elastic.co/5.6.3-d64d30d8
 export ES_BUILD_PKG_EXT=tar
 
 export AIT_ANSIBLE_PLAYBOOK=install_no_xpack
-export AIT_VM=setup_vagrant_vm
+export AIT_VM=vagrant_vm
 
 source jenkins_build.sh
