4c09d26a9e53ec1e7d47ec9c15a0f324690f3688
liza-mae <liza-mae@users.noreply.github.com>
2018-11-29 10:46:03 -0700

    Fix path for playbooks (#52)

--- playbooks/monitoring/ci/jenkins_monitoring.sh
+++ playbooks/monitoring/ci/jenkins_monitoring.sh
@@ -9,7 +9,7 @@ export ES_BUILD_OSS=false
 export ES_BUILD_PKG_EXT=tar
 
 # Install package 
-export AIT_ANSIBLE_PLAYBOOK="${AIT_ROOTDIR}/playbooks/monitoring/docs_parity.yml"
+export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/monitoring/docs_parity.yml"
 
 # Setup VM
 export AIT_VM=vagrant_vm
--- playbooks/stack_testing/ci/jenkins_stack_testing.sh
+++ playbooks/stack_testing/ci/jenkins_stack_testing.sh
@@ -5,9 +5,9 @@
 if [ $CI_BUILD == "true" ]; then
     export ES_BUILD_OSS=$CI_OSS
     if [ $ES_BUILD_OSS == "true" ]; then
-      export AIT_ANSIBLE_PLAYBOOK="${AIT_ROOTDIR}/playbooks/stack_testing/install_no_xpack.yml"
+      export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/stack_testing/install_no_xpack.yml"
     else
-      export AIT_ANSIBLE_PLAYBOOK="${AIT_ROOTDIR}/playbooks/stack_testing/install_xpack.yml"
+      export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/stack_testing/install_xpack.yml"
     fi
     # This will eventually be added to the CI_ stuff
     export ES_BUILD_PKG_EXT=tar
