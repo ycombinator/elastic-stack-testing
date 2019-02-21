3e6bcab095bca27bf9ec3833960277331967dd95
liza-mae <liza-mae@users.noreply.github.com>
2019-02-21 15:49:48 -0700

    Support upgrade variables (#128) (#130)

--- scripts/python/ansible_es_build_vars.py
+++ scripts/python/ansible_es_build_vars.py
@@ -10,7 +10,7 @@ import os
 
 from es_build import ElasticStackBuild
 
-def get_ansible_output(esb):    
+def get_ansible_output(esb):
     ansible_vars = {}
     regex = re.compile('[a-z_]+_package_url')
     attrs = [x for x in ElasticStackBuild.__dict__.keys() if regex.search(x)]
@@ -24,11 +24,14 @@ def get_ansible_output(esb):
         if value:
             ansible_vars.update({prefix + attr: value})
     return ansible_vars
- 
-esb = ElasticStackBuild()
-#TODO: future use: upgrade support 
-#esb = ElasticStackBuild(upgrade=True)
 
+esb = ElasticStackBuild()
 rootdir  = os.getenv('WORKSPACE', '/tmp')
 with open(rootdir + '/vars.yml', 'w') as f:
     yaml.dump(get_ansible_output(esb), f, default_flow_style=False)
+
+if os.getenv('UPGRADE_ES_BUILD_URL'):
+    esb = ElasticStackBuild(upgrade=True)
+    rootdir  = os.getenv('WORKSPACE', '/tmp')
+    with open(rootdir + '/vars.yml', 'a') as f:
+        yaml.dump(get_ansible_output(esb), f, default_flow_style=False)
