af49e1176c48c640b901aa3b917ad9c31709a09c
Shaunak Kashyap <ycombinator@gmail.com>
2019-05-31 07:46:50 -0700

    Run monitoring playbooks separately, per product (#217)
    * Separating into per-product playbooks
    
    * Updating README

--- playbooks/monitoring/ci/jenkins_monitoring.sh
+++ playbooks/monitoring/ci/jenkins_monitoring.sh
@@ -9,7 +9,7 @@ export ES_BUILD_OSS=false
 export ES_BUILD_PKG_EXT=tar
 
 # Install package 
-export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/monitoring/docs_parity.yml"
+export AIT_ANSIBLE_PLAYBOOK="$(pwd)/playbooks/monitoring/${AIT_STACK_PRODUCT}/docs_parity.yml"
 
 # Setup VM
 export AIT_VM=vagrant_vm
