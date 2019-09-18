50e9ab6f9a5857a417f0da11c36437f4a9e5d69c
Shaunak Kashyap <ycombinator@gmail.com>
2019-09-18 08:13:42 -0700

    [6.8] [Stack Monitoring] Implementing diff in parity tests using dictdiffer (#353) (#372)
    * [Stack Monitoring] Implementing diff in parity tests using dictdiffer (#353)
    
    * Implementing diff for beat parity testing using dictdiffer
    
    * WIP: Adding dictdiffer implementation for Kibana
    
    * Handle kibana_settings special case
    
    * Implementing diff for logstash parity testing using dictdiffer
    
    * Implementing diff for elasticsearch parity testing using dictdiffer
    
    * Removing commented out old diff algorithm from kibana
    
    * Extracting more common code to util
    
    * Removing old comment
    
    * Remove Metricbeat-created ILM policy stats
    
    * Replacing jsondiff with dictdiffer in requirements.txt
    
    * Cast iterator to list
    
    * Use beat instead of agent since this is pre 7.0
    
    * Handle cluster_stats special case

--- playbooks/monitoring/common/docs_compare_util.py
+++ playbooks/monitoring/common/docs_compare_util.py
@@ -1,6 +1,23 @@
 import os
 import sys
 import json
+from dictdiffer import diff
+
+allowed_insertions_in_metricbeat_docs = [
+  # 'path.to.field'
+  'service',
+  '@timestamp',
+  'beat',
+  'event',
+  'host',
+  'ecs',
+  'metricset'
+]
+
+allowed_deletions_from_metricbeat_docs = [
+  # 'path.to.field'
+  'source_node',
+]
 
 def log_ok(message):
     sys.stdout.write("OK: " + message + "\n")
@@ -10,7 +27,6 @@ def log_error(message):
 
 def log_parity_error(message):
     log_error(message)
-    sys.exit(21)
     
 def check_usage():
   if (len(sys.argv) < 3):
@@ -101,3 +117,75 @@ def has_deletions_recursive(obj):
     else:
         return False
 
+def make_diff_path(diff_path):
+  if isinstance(diff_path, list):
+    path = ''
+    for item in diff_path:
+      path = path + '.' + str(item)
+    return path.strip('.')
+  else:
+    return diff_path
+
+def check_diff(diff_item, allowed_diffs_in_metricbeat_docs):
+  unexpected_diff_paths = []
+
+  diff_parent_path = make_diff_path(diff_item[1])
+  diff_children = diff_item[2]
+
+  for diff_child in diff_children:
+    diff_path = diff_parent_path + '.' + diff_child[0]
+    diff_path = diff_path.strip('.')
+
+    if diff_path not in allowed_diffs_in_metricbeat_docs:
+      unexpected_diff_paths.append(diff_path)
+
+  return unexpected_diff_paths
+
+def check_parity(handle_special_cases = lambda t, i, m: None, allowed_insertions_in_metricbeat_docs_extra = [], allowed_deletions_from_metricbeat_docs_extra = []):
+  allowed_insertions_in_metricbeat_docs.extend(allowed_insertions_in_metricbeat_docs_extra)
+  allowed_deletions_from_metricbeat_docs.extend(allowed_deletions_from_metricbeat_docs_extra)
+
+  check_usage()
+
+  internal_docs_path = get_internal_docs_path()
+  metricbeat_docs_path = get_metricbeat_docs_path()
+
+  internal_doc_types = get_doc_types(internal_docs_path)
+  metricbeat_doc_types = get_doc_types(metricbeat_docs_path)
+
+  check_num_doc_types(internal_doc_types, metricbeat_doc_types)
+
+  num_errors = 0
+  for doc_type in internal_doc_types:
+      internal_doc = get_doc(internal_docs_path, doc_type)
+      metricbeat_doc = get_doc(metricbeat_docs_path, doc_type)
+
+      handle_special_cases(doc_type, internal_doc, metricbeat_doc)
+
+      unexpected_insertions = []
+      unexpected_deletions = []
+      for diff_item in diff(internal_doc, metricbeat_doc):
+        diff_type = diff_item[0]
+
+        if diff_type == 'add':
+          unexpected_insertions.extend(check_diff(diff_item, allowed_insertions_in_metricbeat_docs))
+
+        if diff_type == 'remove':
+          unexpected_deletions.extend(check_diff(diff_item, allowed_deletions_from_metricbeat_docs))
+
+      if len(unexpected_insertions) == 0 and len(unexpected_deletions) == 0:
+        log_ok("Metricbeat-indexed doc for type='" + doc_type + "' has expected parity with internally-indexed doc.")
+        continue
+
+      if len(unexpected_insertions) > 0:
+        for insertion in unexpected_insertions:
+          log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has unexpected insertion: " + insertion)
+          num_errors = num_errors + 1
+
+      if len(unexpected_deletions) > 0:
+        for deletion in unexpected_deletions:
+          log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has unexpected deletion: " + deletion)
+          num_errors = num_errors + 1
+
+  if num_errors > 0:
+      exit(100 + num_errors)
