cff65dbca6125ea0aefc903fd1976b686e8ce711
Shaunak Kashyap <ycombinator@gmail.com>
2019-09-18 06:56:58 -0700

    [Stack Monitoring] Implementing diff in parity tests using dictdiffer (#353) (#370)
    * Implementing diff for beat parity testing using dictdiffer
    
    * WIP: Adding dictdiffer implementation for Kibana
    
    * Handle kibana_settings special case
    
    * Implementing diff for logstash parity testing using dictdiffer
    
    * Implementing diff for elasticsearch parity testing using dictdiffer
    
    * Removing commented out old diff algorithm from kibana
    
    * Extracting more common code to util
    
    * Removing old comment
    
    * Remove Metricbeat-created ILM policy stats
    
    * Replacing jsondiff with dictdiffer in requirements.txt
    
    * Cast iterator to list

--- playbooks/monitoring/beat/docs_compare.py
+++ playbooks/monitoring/beat/docs_compare.py
@@ -3,55 +3,6 @@ Usage:
   python docs_compare.py /path/to/internal/docs /path/to/metricbeat/docs
 '''
 
-from docs_compare_util import *
-from jsondiff import diff
-import json
+from docs_compare_util import check_parity
 
-check_usage()
-
-internal_docs_path = get_internal_docs_path()
-metricbeat_docs_path = get_metricbeat_docs_path()
-
-internal_doc_types = get_doc_types(internal_docs_path)
-metricbeat_doc_types = get_doc_types(metricbeat_docs_path)
-
-check_num_doc_types(internal_doc_types, metricbeat_doc_types)
-
-for doc_type in internal_doc_types:
-    internal_doc = get_doc(internal_docs_path, doc_type)
-    metricbeat_doc = get_doc(metricbeat_docs_path, doc_type)
-
-    difference = diff(internal_doc, metricbeat_doc, syntax='explicit', marshal=True)
-
-    # Expect there to be exactly seven top-level insertions to the metricbeat-indexed doc: service, beat, agent, @timestamp, host, event, and metricset
-    allowed_insertions = [ "service", "beat", "agent", "@timestamp", "host", "event", "metricset" ]
-    insertions = difference.get('$insert')
-    if insertions == None or len(insertions) < 1:
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has no insertions. Expected up to " + len(allowed_insertions) + " to be inserted.")
-
-    if len(insertions) > len(allowed_insertions):
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has too many insertions: " + json.dumps(insertions))
-
-    difference.pop('$insert') 
-
-    # Expect there to be exactly one top-level deletion from metricbeat-indexed doc: source_node
-    deletions = difference.get('$delete')
-    if deletions == None or len(deletions) < 1:
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has no deletions. Expected 'source_node' to be deleted.")
-
-    if len(deletions) > 1:
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has too many deletions: " + json.dumps(deletions))
-
-    if deletions[0] != 'source_node':
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' does not have 'source_node' deleted.")
-
-    difference.pop('$delete') 
-
-    # Updates are okay in metricbeat-indexed docs, but insertions and deletions are not
-    if has_insertions_recursive(difference):
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has unexpected insertions. Difference: " + json.dumps(difference, indent=2))
-
-    if has_deletions_recursive(difference):
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has unexpected deletions. Difference: " + json.dumps(difference, indent=2))
-
-    log_ok("Metricbeat-indexed doc for type='" + doc_type + "' has expected parity with internally-indexed doc.")
+check_parity()
