6cef6798535a4584d46d6cf2c54599dbb0885cae
Shaunak Kashyap <ycombinator@gmail.com>
2019-09-19 06:12:21 -0700

    [Stack Monitoring] Implementing diff in parity tests using dictdiffer (#353) (#371)
    * Implementing diff for beat parity testing using dictdiffer
    
    * WIP: Adding dictdiffer implementation for Kibana
    
    * Handle kibana_settings special case
    
    * Implementing diff for logstash parity testing using dictdiffer
    
    * Implementing diff for elasticsearch parity testing using dictdiffer
    
    * Removing commented out old diff algorithm from kibana
    
    * Extracting more common code to util
    
    * Removing old comment
    
    * Remove Metricbeat-created ILM policy stats
    
    * Replacing jsondiff with dictdiffer in requirements.txt
    
    * Cast iterator to list

--- playbooks/monitoring/elasticsearch/docs_compare.py
+++ playbooks/monitoring/elasticsearch/docs_compare.py
@@ -3,9 +3,7 @@ Usage:
   python docs_compare.py /path/to/internal/docs /path/to/metricbeat/docs
 '''
 
-from docs_compare_util import *
-from jsondiff import diff
-import json
+from docs_compare_util import check_parity
 
 def handle_special_case_index_recovery(internal_doc, metricbeat_doc):
     # Normalize `index_recovery.shards` array field to have only one object in it.
@@ -13,15 +11,41 @@ def handle_special_case_index_recovery(internal_doc, metricbeat_doc):
     metricbeat_doc["index_recovery"]["shards"] = [ metricbeat_doc["index_recovery"]["shards"][0] ]
 
 def handle_special_case_cluster_stats(internal_doc, metricbeat_doc):
+    # We expect the node ID to be different in the internally-collected vs. metricbeat-collected
+    # docs because the tests spin up a fresh 1-node cluster prior to each type of collection.
+    # So we normalize the node names.
+    new_node_name = '__normalized__'
+
+    orig_node_name = internal_doc['cluster_state']['master_node']
+    internal_doc['cluster_state']['master_node'] = new_node_name
+    internal_doc['cluster_state']['nodes'][new_node_name] = internal_doc['cluster_state']['nodes'][orig_node_name]
+    del internal_doc['cluster_state']['nodes'][orig_node_name]
+
+    orig_node_name = metricbeat_doc['cluster_state']['master_node']
+    metricbeat_doc['cluster_state']['master_node'] = new_node_name
+    metricbeat_doc['cluster_state']['nodes'][new_node_name] = metricbeat_doc['cluster_state']['nodes'][orig_node_name]
+    del metricbeat_doc['cluster_state']['nodes'][orig_node_name]
+
     # When Metricbeat-based monitoring is used, Metricbeat will setup an ILM policy for
     # metricbeat-* indices. Obviously this policy is not present when internal monitoring is
     # used, since Metricbeat is not running in that case. So we normalize by removing the
     # usage stats associated with the Metricbeat-created ILM policy.
-    ilm = metricbeat_doc["stack_stats"]["xpack"]["ilm"]
+    policy_stats = metricbeat_doc["stack_stats"]["xpack"]["ilm"]["policy_stats"]
 
-    ilm["policy_stats"].pop()
-    metricbeat_doc["stack_stats"]["xpack"]["ilm"]["policy_stats"] = ilm["policy_stats"]
-    metricbeat_doc["stack_stats"]["xpack"]["ilm"]["policy_count"] = ilm["policy_count"] - 1
+    # The Metricbeat ILM policy is the one with exactly one phase: hot
+    new_policy_stats = []
+    for policy_stat in policy_stats:
+      policy_phases = list(policy_stat["phases"].keys())
+      num_phases = len(policy_phases)
+      if num_phases != 1:
+        new_policy_stats.append(policy_stat)
+        continue
+      if policy_phases[0] != 'hot':
+        new_policy_stats.append(policy_stat)
+        continue
+
+    metricbeat_doc["stack_stats"]["xpack"]["ilm"]["policy_stats"] = new_policy_stats
+    metricbeat_doc["stack_stats"]["xpack"]["ilm"]["policy_count"] = len(new_policy_stats)
 
 def handle_special_case_node_stats(internal_doc, metricbeat_doc):
     # Metricbeat-indexed docs of `type:node_stats` fake the `source_node` field since its required
@@ -65,54 +89,4 @@ def handle_special_cases(doc_type, internal_doc, metricbeat_doc):
     if doc_type == 'shards':
         handle_special_case_shards(internal_doc, metricbeat_doc)
 
-
-check_usage()
-
-internal_docs_path = get_internal_docs_path()
-metricbeat_docs_path = get_metricbeat_docs_path()
-
-internal_doc_types = get_doc_types(internal_docs_path)
-metricbeat_doc_types = get_doc_types(metricbeat_docs_path)
-
-check_num_doc_types(internal_doc_types, metricbeat_doc_types)
-
-for doc_type in internal_doc_types:
-    internal_doc = get_doc(internal_docs_path, doc_type)
-    metricbeat_doc = get_doc(metricbeat_docs_path, doc_type)
-
-    handle_special_cases(doc_type, internal_doc, metricbeat_doc)
-
-    difference = diff(internal_doc, metricbeat_doc, syntax='explicit', marshal=True)
-
-    # Expect there to be exactly seven top-level insertions to the metricbeat-indexed doc: service, ecs, agent, @timestamp, host, event, and metricset
-    allowed_insertions = [ "service", "ecs", "agent", "@timestamp", "host", "event", "metricset" ]
-    insertions = difference.get('$insert')
-    if insertions == None or len(insertions) < 1:
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has no insertions. Expected 'beat', '@timestamp', 'host', and 'metricset' to be inserted.")
-
-    if len(insertions) > len(allowed_insertions):
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has too many insertions: " + json.dumps(insertions))
-
-    difference.pop('$insert') 
-
-    # Expect there to be exactly one top-level deletion from metricbeat-indexed doc - the `source_node` field - except
-    # if the doc type is `node_stats` or `shards`. Those doc types are expected to contain the `source_node` field
-    if doc_type != 'node_stats' and doc_type != 'shards':
-        deletions = difference.get('$delete')
-        if deletions == None or len(deletions) < 1:
-          # All other types should have source_node deleted from Metricbeat-indexed docs.
-          log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has no deletions. Expected 'source_node' to be deleted.")
-
-        if len(deletions) > 1:
-            log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has too many deletions: " + json.dumps(deletions))
-
-        difference.pop('$delete') 
-
-    # Updates are okay in metricbeat-indexed docs, but insertions and deletions are not
-    if has_insertions_recursive(difference):
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has unexpected insertions. Difference: " + json.dumps(difference, indent=2))
-
-    if has_deletions_recursive(difference):
-        log_parity_error("Metricbeat-indexed doc for type='" + doc_type + "' has unexpected deletions. Difference: " + json.dumps(difference, indent=2))
-
-    log_ok("Metricbeat-indexed doc for type='" + doc_type + "' has expected parity with internally-indexed doc.")
+check_parity(handle_special_cases)
