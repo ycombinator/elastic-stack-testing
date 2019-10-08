1f4460544d0de6044a2b0c0c446f2a7a1e950bbc
liza-mae <liza-mae@users.noreply.github.com>
2019-10-08 14:43:47 -0600

    Increase timeout (#391)

--- ci/cloud/buildSrc/src/main/java/org/estf/gradle/CreateCloudCluster.java
+++ ci/cloud/buildSrc/src/main/java/org/estf/gradle/CreateCloudCluster.java
@@ -60,6 +60,7 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -104,9 +105,11 @@ public class CreateCloudCluster extends DefaultTask {
         // Create cluster
         ClusterCrudResponse response = clusterClient.createEsCluster(createClusterRequest(instanceClient));
         ClustersKibanaApi kbnApi = new ClustersKibanaApi(cloudApi.getApiClient());
+
+        Duration wait = Duration.ofMinutes(15);
         Waiter.waitFor(() -> cloudApi.isKibanaRunning(
             kbnApi.getKibanaCluster(response.getKibanaClusterId(), false, true, false, false)
-        ));
+        ), wait);
 
         // Get cluster info
         clusterId = response.getElasticsearchClusterId();
